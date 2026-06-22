8364093726dc14efce642c260a4226a2e696c040
Josselin <josselin.fauconnier@laplateforme.io>
2026-06-22 16:22:25 +0200

    ok

--- JS/data_trad.js
+++ JS/data_trad.js
@@ -1,13 +1,13 @@
 const trad = {
     fr: {
-        // Messages d'accessibilité / libellés du bouton de langue
+        
         'switch-to-english': 'Passer en anglais',
         'switch-to-dark': 'Passer en mode sombre',
         'switch-to-light': 'Passer en mode clair',
     },
 
     en: {
-        // Accessibility messages / language button labels
+        
         'switch-to-french': 'Switch to French',
         'switch-to-dark': 'Switch to dark mode',
         'switch-to-light': 'Switch to light mode',
